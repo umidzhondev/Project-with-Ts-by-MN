34ca398c8a91e8302319fcc32c3bd683b69b76c8
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-11-07 21:52:14 +0600

    Lesson-55: Added ShopingCart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -30,25 +30,77 @@ export const useShoppingCart = () => {
 
 export const ShoppingCartProvider = ({children}: ShoppingCartProviderProps) => {
     
-    const [isOPen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
     const [cartItems, setCartItems] = useLocalStorage<CartItem[]> (
         "shopping-cart",
         []
     )
     
+    const cartQuantity = cartItems.reduce(
+        (quantity, item) => item.quantity + quantity, 
+        0
+    )
+    
     const openCart = () => setIsOpen(true)
     const closeCart = () => setIsOpen(false)
     
-    function getItemQuantity(id: number) {
-       return cartItems.find(item => item.id ===id) ?.qunatity || 0
+    const getItemQuantity = (id: number) => {
+       return cartItems.find(item => item.id ===id) ?.quantity || 0
+    }
+    
+    const increaseCartQuantity = (id: number) => {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id == id ) == null) {
+                return [...currItems, {id, quantity: 1}]
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return {
+                            ...item, quantity: item.quantity + 1
+                        }
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    } 
+    const decreaseCartQuantity = (id: number) => {
+        setCartItems(currItems => {
+            if (currItems.find(item => item.id == id )?.quantity === 1) {
+                return currItems.filter(item => item.id !== id)
+            } else {
+                return currItems.map(item => {
+                    if (item.id === id) {
+                        return {
+                            ...item, quantity: item.quantity - 1
+                        }
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    }
+    
+    const removeFromCart = (id: number) => {
+        setCartItems(currItems => {
+            return currItems.filter(item => item.id !== id)
+        })
     }
     
     
     return (
         <ShoppingCartContext.Provider 
         value={{
+            getItemQuantity,
+            increaseCartQuantity,
+            decreaseCartQuantity,
+            removeFromCart,
             openCart,
-            closeCart
+            closeCart,
+            cartItems,
+            cartQuantity,
         }}>
             {children}
         </ShoppingCartContext.Provider>
