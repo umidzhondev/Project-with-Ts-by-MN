9fde37e768a8598cb57790c671012c16ecb027cd
umidzhondev <umidzhonfullstackdev@gmail.com>
2022-11-07 21:30:20 +0600

    Lesson-53: Added Navbar and Styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom"; 
+
+import './Home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+       <Container className="h-100">
+           <div className="home-main">
+             <h1 className="home-main-title">Unlimited Training</h1> 
+             <p className='home-main--text'>Extend the limits of your workouts by exercising
+                 your strength, resistance and flexibility where you want
+                 and at the intensity you want</p>  
+           <NavLink to="/store">
+           <button className='home-main--btn'>
+             shop from us
+           </button>
+         </NavLink>
+           </div>
+       </Container>
+    </div>
   )
 }
 
